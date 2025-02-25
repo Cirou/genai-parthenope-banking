aa9935462b87c7d139b8a0d09896d6d80435de08
Ciro Aiello <ciroaiello@live.com>
2025-02-25 22:04:29 +0100

    Aggiornato codice generato per development 6

--- parthenope-banking/src/app/app.routes.ts
+++ parthenope-banking/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
 ];
