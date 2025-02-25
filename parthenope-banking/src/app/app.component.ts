132023a41cccf348dcccafbbf69f94d1b5a48d3b
Ciro Aiello <ciroaiello@live.com>
2025-02-25 20:42:05 +0100

    Aggiornato codice generato per development 5

--- parthenope-banking/src/app/app.component.ts
+++ parthenope-banking/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, LoginComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'parthenope-banking';
-}
