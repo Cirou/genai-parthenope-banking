5b8b1445c4c548aaeae19610d23102cef9627b78
Ciro Aiello <ciroaiello@live.com>
2025-02-25 18:27:17 +0100

    Aggiunto codice generato per development 5

---
+++ parthenope-banking/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
